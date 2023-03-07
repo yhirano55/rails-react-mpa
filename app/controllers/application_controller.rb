c8ae13476f2c8275f90ab2376da330c68017e4e1
Yoshiyuki Hirano <yhirano@me.com>
2023-03-08 01:22:46 +0900

    Rename ReactComponentRenderable -> ReactAppRenderable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::Base
-  include ReactComponentRenderable
+  include ReactAppRenderable
 end
--- app/controllers/concerns/react_app_renderable.rb
+++ app/controllers/concerns/react_app_renderable.rb
@@ -1,4 +1,4 @@
-module ReactComponentRenderable
+module ReactAppRenderable
   extend ActiveSupport::Concern
 
   private
